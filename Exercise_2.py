16d4cd3a8ad6d713fe805bc5ab3793a5c53d90d6
candida17 <53035166+candida17@users.noreply.github.com>
2025-08-15 10:30:05 +0530

    Update Exercise_2.py
    Quick sort - Recursive

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,37 @@
 # Python program for implementation of Quicksort Sort 
   
 # give you explanation for the approach
+
+#Time Complexity
+#Best and AverageCase -> O(n log n) , Worst Case -> O(n²)
+#Space Complexity -> O(log n)
+
 def partition(arr,low,high):
   
   
     #write your code here
+  pivot = arr[high] #choose the last element as pivot
+  i = low - 1
+
+  for j in range(low, high):
+    if arr[j] <= pivot:
+      i += 1
+      arr[i], arr[j] = arr[j], arr[i] #swap
+
+  arr[i+1], arr[high] = arr[high], arr[i+1] #place pivot in correct position
+  return i + 1
+  
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
     
     #write your code here
+  if low < high:
+    pi = partition(arr, low, high) #Partition Index
+    #Recursively sort elements before and after partition
+    quickSort(arr, low, pi-1)
+    quickSort(arr, pi+1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
